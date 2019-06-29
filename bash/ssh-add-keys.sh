885e450f8d6ef5f44275c2b2cc15d4fdaa4cae1d
unicman <unicman@gmail.com>
2019-06-29 17:17:31 +0530

    Support for custom location in addition to ~/.ssh/key.d added with a flag --incremental if earlier keys need to be retained in agent.x

--- bash/ssh-add-keys.sh
+++ bash/ssh-add-keys.sh
@@ -7,58 +7,94 @@
 
 set -e
 
+################################################################################
+# Constants / variables
+################################################################################
+
+
 KEY_DIR=~/.ssh/key.d
+EXTRA_KEY_DIR=
 KEY_EXPIRY=
+CLEAN_SSH_AGENT=1
 
 if [ "$1" != "" ] ; then
     KEY_EXPIRY=" -t $1 "
 fi
 
 if [ "$2" != "" ] ; then
-    KEY_DIR=$2
+    EXTRA_KEY_DIR=$2
 fi
 
-##
-# Add keys with no extension
-##
+if [ "$3" == "--incremental" ] ; then
+    CLEAN_SSH_AGENT=0
+elif [ "$3" == "--clean" ] ; then
+    CLEAN_SSH_AGENT=1
+fi
 
-for keyFile in ${KEY_DIR}/* ; do
-    fileName=$(basename "${keyFile}")
-    if [ -f "${keyFile}" ] && [[ ! "${fileName}" =~ "." ]] ; then
-        ssh-add ${KEY_EXPIRY} "${keyFile}"
-    
-    # Uncomment only for debugging
-    #else
-    #    echo "Skipped ${keyFile}"
+################################################################################
+# Utility functions
+################################################################################
+
+fnSshAdd()
+{
+    INTERNAL_KEY_DIR=$1
+
+    if [ "${INTERNAL_KEY_DIR}" == "" ] ; then
+        return
     fi
-done
 
-##
-# Convert windows keys
-##
+    ##
+    # Add keys with no extension
+    ##
 
-if puttygen --version > /dev/null ; then
-    mkdir -p "${KEY_DIR}/.cache"
-
-    # Create RAM disk to avoid storing passphrase permanently
-    pwdFile=$(mktemp /tmp/skXXXXXX)
-    echo "${KEY_PWD}" > ${pwdFile}
-
-    for ppkFile in ${KEY_DIR}/*.ppk ; do
-        ppkDir=$(dirname "${ppkFile}")
-        fileName=$(basename "${ppkFile}")
-        keyFile="${ppkDir}/.cache/${fileName%.*}"
-
-        # Convert if not already converted...
-        if [ ! -f "${keyFile}" ] && [ -f "${ppkFile}" ] ; then
-            echo "Converting ${ppkFile}..."
-            puttygen "${ppkFile}" -O private-openssh -o "${keyFile}" --new-passphrase ${pwdFile}
-            chmod 600 "${keyFile}"
+    for keyFile in ${INTERNAL_KEY_DIR}/* ; do
+        fileName=$(basename "${keyFile}")
+        if [ -f "${keyFile}" ] && [[ ! "${fileName}" =~ "." ]] ; then
+            ssh-add ${KEY_EXPIRY} "${keyFile}"
+        
+        # Uncomment only for debugging
+        #else
+        #    echo "Skipped ${keyFile}"
         fi
-
-        ssh-add ${KEY_EXPIRY} "${keyFile}"
     done
 
-    rm -f ${pwdFile}
+    ##
+    # Convert windows keys
+    ##
+
+    if puttygen --version > /dev/null ; then
+        mkdir -p "${INTERNAL_KEY_DIR}/.cache"
+
+        # Create RAM disk to avoid storing passphrase permanently
+        pwdFile=$(mktemp /tmp/skXXXXXX)
+        echo "${KEY_PWD}" > ${pwdFile}
+
+        for ppkFile in ${INTERNAL_KEY_DIR}/*.ppk ; do
+            ppkDir=$(dirname "${ppkFile}")
+            fileName=$(basename "${ppkFile}")
+            keyFile="${ppkDir}/.cache/${fileName%.*}"
+
+            # Convert if not already converted...
+            if [ ! -f "${keyFile}" ] && [ -f "${ppkFile}" ] ; then
+                echo "Converting ${ppkFile}..."
+                puttygen "${ppkFile}" -O private-openssh -o "${keyFile}" --new-passphrase ${pwdFile}
+                chmod 600 "${keyFile}"
+            fi
+
+            ssh-add ${KEY_EXPIRY} "${keyFile}"
+        done
+
+        rm -f ${pwdFile}
+    fi
+}
+
+################################################################################
+# Main logic
+################################################################################
+
+if [ "${CLEAN_SSH_AGENT}" == "1" ] ; then
+    ssh-add -D
 fi
 
+fnSshAdd "${KEY_DIR}"
+fnSshAdd "${EXTRA_KEY_DIR}"
